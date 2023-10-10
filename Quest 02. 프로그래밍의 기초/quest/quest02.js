3a86a2c9b573c2a4013ea0a589730fd52e528584
DESKTOP-H4DS8GO\USER <daniel.l@kakao.com>
2023-10-10 17:43:00 +0900

    Quest2

---
+++ Quest 02. 프로그래밍의 기초/quest/quest02.js
@@ -0,0 +1,16 @@
+
+const height = 5;
+
+for (let i = 1; i <= height; i++) {
+    let row = '';
+    
+    for (let j = 1; j <= height - i; j++) {
+        row += ' ';
+    }
+    
+    for (let k = 1; k <= 2 * i - 1; k++) {
+        row += '*';
+    }
+    
+    console.log(row);
+}
